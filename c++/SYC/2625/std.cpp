38223e4f07b1c428150fa0aebd8a0ff3c5eec81c
Phantom-HY <hyhh1209@hotmail.com>
2023-08-17 11:09:41 +0800

    Python缩进2625

---
+++ c++/SYC/2625/std.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+char a[7005];
+int sum[7005];
+int dp[7005][7005];
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("data.in", "r", stdin);
+    // freopen("data.out", "w", stdout);
+#endif
+    scanf("%d\n", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%c\n", a + i);
+    for (int i = n; i >= 1; i--)
+    {
+        
+    }
+    return 0;
+}
