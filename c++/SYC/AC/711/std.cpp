3a17ffe191603a57f5d2852080758eb0666e4376
Phantom-HY <hyhh1209@hotmail.com>
2023-10-28 22:32:17 +0800

    SYC-AC-711

---
+++ c++/SYC/AC/711/std.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, m;
+int a[100005];
+int x, y;
+int st[100005][25];
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("data.in", "r", stdin);
+    // freopen("data.out", "w", stdout);
+#endif
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", a + i);
+    for (int j = 0; j <= 20; j++)
+        for (int i = 1; i + (1 << j) - 1 <= n; i++)
+            if (j == 0)
+                st[i][j] = a[i];
+            else
+                st[i][j] = max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+    while (m--)
+    {
+        scanf("%d%d", &x, &y);
+        int k = log2(y - x + 1);
+        printf("%d\n", max(st[x][k], st[y - (1 << k) + 1][k]));
+    }
+    return 0;
+}
